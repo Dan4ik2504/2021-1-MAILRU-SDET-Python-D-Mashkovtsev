66ce82add490d776a6475277b7b49eee15866ea1
Dan4ik2504 <dan4ik2504@gmail.com>
2021-03-24 23:00:42 +0300

    Added fixture 'driver'

--- Homework_1/conftest.py
+++ Homework_1/conftest.py
@@ -1,1 +1,11 @@
+import pytest
+from selenium import webdriver
 
+import settings
+
+
+@pytest.fixture(scope='function')
+def driver():
+    with webdriver.Chrome(executable_path=settings.DRIVER_PATH) as driver:
+        driver.maximize_window()
+        yield driver
