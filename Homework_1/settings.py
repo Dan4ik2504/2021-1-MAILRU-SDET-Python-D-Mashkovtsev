9c1130fd4a78a3f99f6b50bf7c4dbdf5f12f2521
Dan4ik2504 <dan4ik2504@gmail.com>
2021-03-25 20:51:48 +0300

    Refactoring settings and locators

--- Homework_1/settings.py
+++ Homework_1/settings.py
@@ -7,14 +7,24 @@ CLICK_RETRY = 3
 
 # URLs
 BASE_URL = "https://target.my.com/"
-DASHBOARD_URL = BASE_URL + "dashboard"
-SEGMENTS_URL = BASE_URL + "segments"
-BILLING_URL = BASE_URL + "billing"
-STATISTICS_URL = BASE_URL + "statistics"
-PRO_URL = BASE_URL + "pro"
-PROFILE_URL = BASE_URL + "profile"
-TOOLS_URL = BASE_URL + "tools"
-HELP_URL = BASE_URL + "help/advertisers/ru"
+URL_PATHS = {
+    "dashboard": "dashboard",
+    "segments": "segments",
+    "billing": "billing",
+    "statistics": "statistics",
+    "pro": "pro",
+    "profile": "profile",
+    "tools": "tools",
+    "help": "help/advertisers/ru",
+}
+DASHBOARD_URL = BASE_URL + URL_PATHS["dashboard"]
+SEGMENTS_URL = BASE_URL + URL_PATHS["segments"]
+BILLING_URL = BASE_URL + URL_PATHS["billing"]
+STATISTICS_URL = BASE_URL + URL_PATHS["statistics"]
+PRO_URL = BASE_URL + URL_PATHS["pro"]
+PROFILE_URL = BASE_URL + URL_PATHS["profile"]
+TOOLS_URL = BASE_URL + URL_PATHS["tools"]
+HELP_URL = BASE_URL + URL_PATHS["help"]
 
 # User data
 LOGIN = "tebivan222@bombaya.com"
--- Homework_1/ui/locators/pages_locators.py
+++ Homework_1/ui/locators/pages_locators.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 
+import settings
+
 
 class MainPageNoAuth:
     LOGIN_BUTTON = (By.CSS_SELECTOR, "[class^='responseHead-module-button-']")
@@ -18,14 +20,14 @@ class BasePageAuth:
 
     class NavPanel:
         _BASE_XPATH = "//a[contains(@class, 'center-module-button-') and contains(@href, '{}')]"
-        DASHBOARD = (By.XPATH, _BASE_XPATH.format("/dashboard"))
-        SEGMENTS = (By.XPATH, _BASE_XPATH.format("/segments"))
-        BILLING = (By.XPATH, _BASE_XPATH.format("/billing"))
-        STATISTICS = (By.XPATH, _BASE_XPATH.format("/statistics"))
-        PRO = (By.XPATH, _BASE_XPATH.format("/pro"))
-        PROFILE = (By.XPATH, _BASE_XPATH.format("/profile"))
-        TOOLS = (By.XPATH, _BASE_XPATH.format("/tools"))
-        HELP = (By.XPATH, _BASE_XPATH.format("/help/advertisers/ru"))
+        DASHBOARD = (By.XPATH, _BASE_XPATH.format("/" + settings.URL_PATHS["dashboard"]))
+        SEGMENTS = (By.XPATH, _BASE_XPATH.format("/" + settings.URL_PATHS["segments"]))
+        BILLING = (By.XPATH, _BASE_XPATH.format("/" + settings.URL_PATHS["billing"]))
+        STATISTICS = (By.XPATH, _BASE_XPATH.format("/" + settings.URL_PATHS["statistics"]))
+        PRO = (By.XPATH, _BASE_XPATH.format("/" + settings.URL_PATHS["pro"]))
+        PROFILE = (By.XPATH, _BASE_XPATH.format("/" + settings.URL_PATHS["profile"]))
+        TOOLS = (By.XPATH, _BASE_XPATH.format("/" + settings.URL_PATHS["tools"]))
+        HELP = (By.XPATH, _BASE_XPATH.format("/" + settings.URL_PATHS["help"]))
 
 
 class ProfilePage(BasePageAuth):
