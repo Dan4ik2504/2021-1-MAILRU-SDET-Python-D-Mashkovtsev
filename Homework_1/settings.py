ae0a34df6685d77df5d09065b83c2bb6b97fd554
Dan4ik2504 <dan4ik2504@gmail.com>
2021-03-25 14:57:31 +0300

    Settings updated

--- Homework_1/settings.py
+++ Homework_1/settings.py
@@ -2,8 +2,8 @@
 DRIVER_PATH = "/opt/WebDriver/bin/chromedriver"
 
 # Basic settings
-BASE_URL = "https://target.my.com"
-DASHBOARD_URL = BASE_URL + "/dashboard"
+BASE_URL = "https://target.my.com/"
+DASHBOARD_URL = BASE_URL + "dashboard"
 DEFAULT_TIMEOUT = 10
 CLICK_RETRY = 3
 
@@ -11,5 +11,3 @@ CLICK_RETRY = 3
 LOGIN = "tebivan222@bombaya.com"
 PASSWORD = "Qwerty123456"
 USERNAME = "Тестов Тест Тестович"
-
-LOGIN_BUTTON_TEXT = "Войти"
