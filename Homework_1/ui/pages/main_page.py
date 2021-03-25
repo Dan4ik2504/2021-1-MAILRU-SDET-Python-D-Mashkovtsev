1514520d90dd17bb95bed0602c9bc31cf7915b52
Dan4ik2504 <dan4ik2504@gmail.com>
2021-03-25 14:28:27 +0300

    Main page object and authorization checks updated

--- Homework_1/ui/pages/main_page.py
+++ Homework_1/ui/pages/main_page.py
@@ -9,26 +9,47 @@ from time import sleep
 
 
 class MainPage(BasePage):
+    
+    locators = pages_locators.MainPage
+
+    class LoginException(Exception):
+        """Пользователь не авторизован"""
+
+    class LogoutException(Exception):
+        """Пользователь авторизован"""
+
     def login(self, login=settings.LOGIN, password=settings.PASSWORD):
         """Авторизация"""
         self.open_page()
-        self.click(pages_locators.MainPage.LOGIN_BUTTON)
-        self.wait().until(EC.visibility_of_element_located(pages_locators.MainPage.AUTH_FORM))
-        self.fill_field(pages_locators.MainPage.EMAIL_FIELD, login)
-        self.fill_field(pages_locators.MainPage.PASSWORD_FIELD, password)
-        self.click(pages_locators.MainPage.LOGIN_CONFIRM_BUTTON)
-        self.is_authorised()
+        self.click(self.locators.LOGIN_BUTTON)
+        self.wait().until(EC.visibility_of_element_located(self.locators.AUTH_FORM))
+        self.fill_field(self.locators.EMAIL_FIELD, login)
+        self.fill_field(self.locators.PASSWORD_FIELD, password)
+        self.click(self.locators.LOGIN_CONFIRM_BUTTON)
+        self.is_authorized(open_new_tab=False)
 
-    def is_authorised(self):
+    def is_authorized(self, open_new_tab=True):
         """Проверка того, что пользователь авторизован"""
-        with self.NewTab(self.driver):
-            self.open_page()
-            self.wait().until(EC.url_changes(settings.BASE_URL))
-            assert self.driver.current_url == settings.DASHBOARD_URL
-            assert self.find(pages_locators.Dashboard.HEADER_USERNAME).text == settings.USERNAME.upper()
 
-    def is_not_authorised(self):
+        def check_func(self, open_new_tab=True, *args, **kwargs):
+            if open_new_tab:
+                self.open_page()
+                self.wait().until(EC.url_changes(settings.BASE_URL))
+            if self.driver.current_url != settings.DASHBOARD_URL:
+                raise self.LoginException("{} != {}", self.driver.current_url, settings.DASHBOARD_URL)
+            return True
+
+        self.checking_in_new_tab_wrapper(check_func, open_new_tab=open_new_tab)
+
+    def is_not_authorized(self, open_new_tab=True):
         """Проверка того, что пользователь не авторизован"""
-        with self.NewTab(self.driver):
-            self.open_page()
-            self.wait().until(EC.visibility_of_element_located(pages_locators.MainPage.LOGIN_BUTTON))
+
+        def check_func(self, open_new_tab=True, *args, **kwargs):
+            if open_new_tab:
+                self.open_page()
+                self.wait().until(EC.visibility_of_element_located(self.locators.LOGIN_BUTTON))
+            if self.driver.current_url != settings.BASE_URL:
+                raise self.LogoutException("{} != {}", self.driver.current_url, settings.BASE_URL)
+            return True
+
+        self.checking_in_new_tab_wrapper(check_func, open_new_tab=open_new_tab)
