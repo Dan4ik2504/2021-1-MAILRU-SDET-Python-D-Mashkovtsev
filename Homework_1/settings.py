523764b685f17336b3c480cfab00f73987ba5519
Dan4ik2504 <dan4ik2504@gmail.com>
2021-03-25 19:17:59 +0300

    Added new URLs and user data to settings.py

--- Homework_1/settings.py
+++ Homework_1/settings.py
@@ -2,12 +2,23 @@
 DRIVER_PATH = "/opt/WebDriver/bin/chromedriver"
 
 # Basic settings
-BASE_URL = "https://target.my.com/"
-DASHBOARD_URL = BASE_URL + "dashboard"
 DEFAULT_TIMEOUT = 10
 CLICK_RETRY = 3
+
+# URLs
+BASE_URL = "https://target.my.com/"
+DASHBOARD_URL = BASE_URL + "dashboard"
+SEGMENTS_URL = BASE_URL + "segments"
+BILLING_URL = BASE_URL + "billing"
+STATISTICS_URL = BASE_URL + "statistics"
+PRO_URL = BASE_URL + "pro"
+PROFILE_URL = BASE_URL + "profile"
+TOOLS_URL = BASE_URL + "tools"
+HELP_URL = BASE_URL + "help/advertisers/ru"
 
 # User data
 LOGIN = "tebivan222@bombaya.com"
 PASSWORD = "Qwerty123456"
 USERNAME = "Тестов Тест Тестович"
+PHONE = "+70000000000"
+EMAIL = "qwertyuiop@bombaya.com"
