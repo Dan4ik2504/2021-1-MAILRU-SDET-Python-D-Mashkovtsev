dc874e1f6cb3496613328a52ccd14a8dbfacbb9c
Dan4ik2504 <dan4ik2504@gmail.com>
2021-03-25 14:58:39 +0300

    Logout test created. Login test updated.

--- Homework_1/base_tests/test_UI.py
+++ Homework_1/base_tests/test_UI.py
@@ -5,7 +5,14 @@ from ui.locators import pages_locators
 from selenium.webdriver.common.by import By
 
 
-class TestUI(BaseCase):
+class TestLoginLogout(BaseCase):
     @pytest.mark.ui
     def test_login(self):
+        self.main_page.login(checking=False)
+        assert self.driver.current_url == settings.DASHBOARD_URL
+
+    @pytest.mark.ui
+    def test_logout(self):
         self.main_page.login()
+        self.main_page.logout(checking=False)
+        assert self.driver.current_url == settings.BASE_URL
