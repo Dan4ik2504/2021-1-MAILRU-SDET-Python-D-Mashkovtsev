7445f55b567f26965315dcfad8a7c874d18f41fb
Dan4ik2504 <dan4ik2504@gmail.com>
2021-03-25 13:06:26 +0300

    A class NewTab, designed to create a new browser tab, has been added to the base page object.

--- Homework_1/ui/pages/base_page.py
+++ Homework_1/ui/pages/base_page.py
@@ -43,3 +43,26 @@ class BasePage:
         element.clear()
         element.send_keys(text)
 
+    class NewTab:
+        """Новая вкладка"""
+        def __init__(self, driver):
+            self.driver = driver
+            self.original_tab = self.driver.current_window_handle
+
+        def open(self):
+            """Открыть вкладку"""
+            self.driver.execute_script("window.open('');")
+            self.new_tab = self.driver.window_handles[-1]
+            self.driver.switch_to.window(self.new_tab)
+            return self.new_tab
+
+        def close(self):
+            """Закрыть вкладку"""
+            self.driver.close()
+            self.driver.switch_to.window(self.original_tab)
+
+        def __enter__(self):
+            return self.open()
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self.close()
