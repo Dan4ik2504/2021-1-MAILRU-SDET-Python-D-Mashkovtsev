217069bcb4559ad0bea538cfd5106029797364c5
Dan4ik2504 <dan4ik2504@gmail.com>
2021-05-07 14:57:33 +0400

    Add more test passing checks

--- Homework_6/tests_database/test_database.py
+++ Homework_6/tests_database/test_database.py
@@ -8,28 +8,52 @@ class TestLoggerDataWriteInDB(DBTestsBase):
     def test_number_of_requests(self):
         response = log_analyzer.number_of_requests()
         self.database_builder.create_entry_number_of_requests(response)
-        assert self.database.session.query(models.NumberOfRequests).count() >= 1
+
+        table = models.NumberOfRequests
+        query = self.database.session.query(table)
+        assert query.count() > 0
+        assert query.filter(table.count == response).count() > 0
 
     def test_number_of_requests_by_type(self):
         response = log_analyzer.number_of_requests_by_type()
         for entry in response:
             self.database_builder.create_entry_number_of_requests_by_type(**entry)
-        assert self.database.session.query(models.NumberOfRequestsByType).count() >= len(response)
+
+        table = models.NumberOfRequestsByType
+        query = self.database.session.query(table)
+        assert query.count() >= len(response)
+        for entry in response:
+            assert query.filter_by(**entry).count() > 0
 
     def test_most_frequent_requests(self):
         response = log_analyzer.most_frequent_requests()
         for entry in response:
             self.database_builder.create_entry_most_frequent_requests(**entry)
-        assert self.database.session.query(models.MostFrequentRequests).count() >= len(response)
+
+        table = models.MostFrequentRequests
+        query = self.database.session.query(table)
+        assert query.count() >= len(response)
+        for entry in response:
+            assert query.filter_by(**entry).count() > 0
 
     def test_largest_requests(self):
         response = log_analyzer.largest_requests()
         for entry in response:
             self.database_builder.create_entry_largest_requests(**entry)
-        assert self.database.session.query(models.LargestRequests).count() >= len(response)
+
+        table = models.LargestRequests
+        query = self.database.session.query(table)
+        assert query.count() >= len(response)
+        for entry in response:
+            assert query.filter_by(**entry).count() > 0
 
     def test_users_by_number_of_requests(self):
         response = log_analyzer.users_by_number_of_requests()
         for entry in response:
             self.database_builder.create_entry_users_by_number_of_requests(**entry)
-        assert self.database.session.query(models.UsersByNumberOfRequests).count() >= len(response)
+
+        table = models.UsersByNumberOfRequests
+        query = self.database.session.query(table)
+        assert query.count() >= len(response)
+        for entry in response:
+            assert query.filter_by(**entry).count() > 0
