c4dbcc70fb908c439ac06174263bd9871ad6135e
Dan4ik2504 <dan4ik2504@gmail.com>
2021-05-18 23:36:24 +0300

    Add logging in mock http client

--- Homework_7/network/mock_http_client.py
+++ Homework_7/network/mock_http_client.py
@@ -1,8 +1,9 @@
-from urllib.parse import urljoin
+import logging
 
 from network.http_client import HttpClient
 import settings
-import exceptions
+
+logger = logging.getLogger(settings.LOGGING.LOGGER_NAME)
 
 
 class MockHTTPClient:
@@ -13,20 +14,24 @@ class MockHTTPClient:
         self.http_client = HttpClient()
 
     def get_user_last_name(self, first_name):
+        logger.info(f'Getting the last name of the user with the first name "{first_name}"')
         response = self.http_client.get(self.url_base.format(str(first_name)))
         return response
 
     def set_user_last_name(self, last_name, first_name):
+        logger.info(f'Setting the last name of the user with the first name "{first_name}"')
         json_data = {'first_name': first_name,
                      'last_name': last_name}
         response = self.http_client.post(self.url, json=json_data)
         return response
 
     def update_user_last_name(self, first_name, last_name):
+        logger.info(f'Updating the last name of the user with the first name "{first_name}"')
         json_data = {'last_name': last_name}
         response = self.http_client.put(self.url_base.format(str(first_name)), json=json_data)
         return response
 
     def delete_user_last_name(self, first_name):
+        logger.info(f'Removing the last name of the user with the first name "{first_name}"')
         response = self.http_client.delete(self.url_base.format(str(first_name)))
         return response
