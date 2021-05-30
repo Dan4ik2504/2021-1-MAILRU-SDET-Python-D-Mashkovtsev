84cb553bd5cea6cb32c69d6da022005f23002d2f
Dan4ik2504 <dan4ik2504@gmail.com>
2021-05-30 17:03:40 +0300

    Update running tests via docker compose

--- Final_project/start.sh
+++ Final_project/start.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 docker network create myapp_network
-docker-compose up
+docker-compose up -d
+
+cd tests || exit
+docker-compose up -d
+cd code || exit
+
+pytest -lsv -n4 --selenoid_vnc
