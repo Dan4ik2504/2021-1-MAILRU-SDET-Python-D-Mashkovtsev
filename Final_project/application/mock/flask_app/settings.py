8a86940e9483e4a0b4c3bf14b92f1462118201dc
Dan4ik2504 <dan4ik2504@gmail.com>
2021-06-09 00:26:41 +0400

    Fix bugs with parallel run

--- Final_project/application/mock/flask_app/settings.py
+++ Final_project/application/mock/flask_app/settings.py
@@ -1,6 +1,5 @@
 from logging import INFO as LOG_LEVEL_INFO
 from utils.paths import paths
-import os
 
 BASE_URL = 'http://localhost'
 
@@ -9,14 +8,14 @@ BASE_URL = 'http://localhost'
 
 class DATABASE_SETTINGS:
     HOST = 'myapp_db'
-    PORT = os.environ.get("MYAPP_DB_PORT", 3306)
+    PORT = 3306
     USER = 'root'
     PASSWORD = 'pass'
 
 
 class MOCK_SETTINGS:
     HOST = '0.0.0.0'
-    PORT = os.environ.get("VK_API_PORT", '8008')
+    PORT = '8008'
     URL = f'{BASE_URL}:{PORT}'
     DB_NAME = 'vk_api_db'
     TABLE_VK_ID_NAME = 'vk_id_table'
@@ -24,5 +23,5 @@ class MOCK_SETTINGS:
 
 class APP_SETTINGS:
     HOST = '0.0.0.0'
-    PORT = os.environ.get("MYAPP_PROXY_PORT", '8070')
+    PORT = '8070'
     URL = f'{BASE_URL}:{PORT}'
